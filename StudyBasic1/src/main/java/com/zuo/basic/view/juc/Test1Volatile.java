1842810c36a32a47b3b401a660edb8ae45fa9f0d
zuo <lipingtso@163.com>
2021-06-28 21:24:25 +0800

    验证volatile特性二：不保证原子性

--- StudyBasic1/src/main/java/com/zuo/basic/view/juc/Test1Volatile.java
+++ StudyBasic1/src/main/java/com/zuo/basic/view/juc/Test1Volatile.java
@@ -1,6 +1,7 @@
 package com.zuo.basic.view.juc;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * volatile三大特性
@@ -10,7 +11,7 @@ import java.util.concurrent.TimeUnit;
  */
 
 /**
- * 2~4课程讲解volatile特性一保证可见性
+ * done 2~4课程讲解volatile特性一:保证可见性
  * zlp studies on 2021/6/20
  */
 public class Test1Volatile {
@@ -83,7 +84,7 @@ class MyData1 {
 /**
  * number 变量添加volatile关键字修饰
  */
-class MyData2 {
+class MyData2 {//MyData2.java --> MyData2.class --> JVM字节码
     //共享变量
     volatile int number = 0;
 
@@ -91,8 +92,53 @@ class MyData2 {
         this.number = 60;
     }
 
+    public void addPlusPlus() {
+        number++;
+    }
+
+    AtomicInteger atomicInteger = new AtomicInteger();
+
+    public void addAtomic() {
+        atomicInteger.getAndIncrement();
+    }
+
 }
 
 /**
- * todo 5~7课程讲解volatile特性一保证可见性
+ * done 5~7课程验证volatile特性二：不保证原子性
+ * 为什么number++在多线程的情况下会出现这种现象
+ * 原理：由于number++底层的操作是由三步完成的（1.获得初始值，2.加1操作，3.把累加的值写回），多线程情况下可能会被加塞，值改变了也没来得及通知子线程，出现丢失写值的情况
+ * 解决方案：1.加sync
+ * 2.使用juc下的AtomicInteger（底层原理：CAS-->compare and swap）
+ */
+class Test3Volatile {
+    public static void main(String[] args) {
+        MyData2 myData2 = new MyData2();
+
+        for (int i = 1; i <= 20; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 1000; j++) {
+                    //不保证原子性
+                    myData2.addPlusPlus();
+                    //保证原子性的解决方案(使用AtomicInteger)
+                    myData2.addAtomic();
+                }
+            }, String.valueOf(i)).start();
+        }
+
+        while (Thread.activeCount() > 2) {
+            Thread.yield();
+        }
+
+        System.out.println(Thread.currentThread().getName() + "\t int type finally number value: " + myData2.number);
+        System.out.println(Thread.currentThread().getName() + "\t AtomicInteger type finally number value: " + myData2.atomicInteger);
+    }
+
+    public static void seeOkVolatile() {
+
+    }
+}
+
+/**
+ * todo volatile指令重排案例1、2
  */
